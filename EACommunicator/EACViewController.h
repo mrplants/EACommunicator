458cab65415131540583a872352e58e6689f31ec
Sean T Fitzgerald <fitzgerald.sean.t@gmail.com>
2013-05-22 08:24:13 -0400

    Initial Commit

---
+++ EACommunicator/EACViewController.h
@@ -0,0 +1,13 @@
+//
+//  EACViewController.h
+//  EACommunicator
+//
+//  Created by Sean Fitzgerald on 5/22/13.
+//  Copyright (c) 2013 Museum of Science Boston. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EACViewController : UIViewController
+
+@end
---
+++ EACommunicator/EACViewController.m
@@ -0,0 +1,29 @@
+//
+//  EACViewController.m
+//  EACommunicator
+//
+//  Created by Sean Fitzgerald on 5/22/13.
+//  Copyright (c) 2013 Museum of Science Boston. All rights reserved.
+//
+
+#import "EACViewController.h"
+
+@interface EACViewController ()
+
+@end
+
+@implementation EACViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
