53f872b5f9e2db8798a7f97829dc74e2838f3c97
Sean T Fitzgerald <fitzgerald.sean.t@gmail.com>
2013-05-23 10:40:43 -0400

    Added zbar and audio messages to project.

---
+++ EACommunicator/EACInstructionViewController.m
@@ -0,0 +1,115 @@
+//
+//  EACInstructionViewController.m
+//  EACommunicator
+//
+//  Created by Sean Fitzgerald on 5/22/13.
+//  Copyright (c) 2013 Museum of Science Boston. All rights reserved.
+//
+
+#import "EACInstructionViewController.h"
+#import <QuartzCore/QuartzCore.h>
+
+@interface EACInstructionViewController ()<UIScrollViewDelegate>
+
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
+
+@property (weak, nonatomic) IBOutlet UIImageView *instructionImageView;
+
+@property (weak, nonatomic) IBOutlet UIPageControl *instructionPageControl;
+
+@property (nonatomic, strong) NSArray * instructionImageArray;
+
+@end
+
+@implementation EACInstructionViewController
+
+//remove this with the back button
+- (IBAction)goBackButton
+{
+	[self dismissViewControllerAnimated:YES completion:NULL];
+}
+
+-(void)viewDidLoad
+{
+	[super viewDidLoad];
+	self.instructionImageArray = @[[UIImage imageNamed:@"instructional cards 1.png"],
+																[UIImage imageNamed:@"instructional cards 2.png"],
+																[UIImage imageNamed:@"instructional cards 3.png"]];
+	self.instructionPageControl.numberOfPages = [self.instructionImageArray count];
+	self.instructionPageControl.currentPage = 0;
+}
+
+-(void)viewDidAppear:(BOOL)animated
+{
+	[super viewDidAppear:animated];
+	CGRect mainFrame = self.scrollView.frame;
+	[self.scrollView setFrame:CGRectMake(self.scrollView.frame.size.width,
+																			0,
+																			mainFrame.size.width,
+																			mainFrame.size.height)];
+	[self setupScrollView];
+	[UIView animateWithDuration:0.5 animations:^() {
+		self.scrollView.frame = mainFrame;
+	}];
+}
+
+-(void)setupScrollView
+{
+	self.scrollView.pagingEnabled = YES;
+	self.scrollView.contentSize = CGSizeMake(CGRectGetWidth(self.scrollView.frame) * [self.instructionImageArray count],
+																					 CGRectGetHeight(self.scrollView.frame));
+	int imageCounter = 0;
+	
+	[self.instructionImageView removeFromSuperview];
+	for (UIImage* instructionImage in self.instructionImageArray)
+	{
+		//create the new imageView
+		UIImageView* instructionImageView = [[UIImageView alloc] initWithImage:instructionImage];
+		
+		//create the correct frame for the imageview.
+		[instructionImageView setFrame:CGRectMake(self.instructionImageView.frame.origin.x + self.scrollView.frame.size.width * imageCounter,
+																							self.instructionImageView.frame.origin.y,
+																							self.instructionImageView.frame.size.width,
+																							self.instructionImageView.frame.size.height)];
+		
+		//make it look nice with rounded corners
+		instructionImageView.layer.cornerRadius = 20.0;
+		instructionImageView.layer.masksToBounds = YES;
+				
+		//add it as a subview
+		[self.scrollView addSubview:instructionImageView];
+    imageCounter++;
+	}
+	
+	self.scrollView.scrollsToTop = NO;
+	self.scrollView.delegate = self;
+}
+
+// at the end of scroll animation, reset the boolean used when scrolls originate from the UIPageControl
+- (void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView
+{
+	// switch the indicator when more than 50% of the previous/next page is visible
+	CGFloat pageWidth = CGRectGetWidth(self.scrollView.frame);
+	NSUInteger page = floor((self.scrollView.contentOffset.x - pageWidth / 2) / pageWidth) + 1;
+	self.instructionPageControl.currentPage = page;
+}
+
+- (void)gotoPage:(BOOL)animated
+{
+	NSInteger page = self.instructionPageControl.currentPage;
+		
+	// update the scroll view to the appropriate page
+	CGRect bounds = self.scrollView.bounds;
+	bounds.origin.x = CGRectGetWidth(bounds) * page;
+	bounds.origin.y = 0;
+	[self.scrollView scrollRectToVisible:bounds animated:animated];
+}
+
+- (IBAction)changePage:(id)sender
+{
+	[self gotoPage:YES];    // YES = animate
+}
+
+
+
+@end
--- EACommunicator/EACScannerViewController.m
+++ EACommunicator/EACScannerViewController.m
@@ -7,18 +7,30 @@
 //
 
 #import "EACScannerViewController.h"
+#import "Constants.h"
+#include "TargetConditionals.h"
+#import <AVFoundation/AVFoundation.h>
+#import <CoreVideo/CoreVideo.h>
+#import "ZBarSDK.h"
 
-@interface EACScannerViewController ()
+@interface EACScannerViewController () <ZBarReaderViewDelegate>
 
+//image Views
 @property (weak, nonatomic) IBOutlet UIImageView *backgroundImageView;
 @property (weak, nonatomic) IBOutlet UIImageView *crosshairsImageView;
 @property (weak, nonatomic) IBOutlet UIImageView *scannerLightImageView;
 
+//visual models
 @property (nonatomic) int crosshairImageIndex;
 @property (strong, nonatomic) NSArray *crosshairImageArray;
 
 @property (nonatomic) int scannerLightImageIndex;
 @property (strong, nonatomic) NSArray *scannerLightImageArray;
+
+//camera equipment
+@property (weak, nonatomic) IBOutlet ZBarReaderView *zBarReaderView;
+
+@property BOOL isProcessingSampleFrame;
 
 @end
 
@@ -27,6 +39,8 @@
 
 -(void)viewDidLoad
 {
+	[super viewDidLoad];
+
 	self.crosshairImageArray =
 	@[[UIImage imageNamed:@"scanner-crosshairs-blank.png"],
 	 [UIImage imageNamed:@"scanner-crosshairs-green.png"],
@@ -38,7 +52,34 @@
 	 [UIImage imageNamed:@"scanner-light-green.png"],
 	 [UIImage imageNamed:@"scanner-light-red.png"]
 	 ];
-	[super viewDidLoad];
+}
+
+-(void)viewDidLayoutSubviews
+{
+	[super viewDidLayoutSubviews];
+	if (IS_IPHONE_5)
+	{
+		[self iPhone5Setup];
+	}
+
+#if !(TARGET_IPHONE_SIMULATOR)
+	[ZBarReaderView class];
+	[self setupZBar];
+#endif
+
+}
+
+-(void) setupZBar
+{
+	// the delegate receives decode results
+	self.zBarReaderView.readerDelegate = self;	
+}
+
+-(void)iPhone5Setup
+{//The iphone 5 has a longer screen, which cannot be accounted for in the storyboard setup, so we need to give it a different image and lay out the sub-images differently
+	self.backgroundImageView.image = [UIImage imageNamed:@"scanner screen-tall@2x.png"];
+	//move the dynamic images down so they still line up with the slots in the background image
+	self.crosshairsImageView.frame = self.scannerLightImageView.frame = CGRectMake(0, 44, self.crosshairsImageView.frame.size.width, self.crosshairsImageView.frame.size.height);
 }
 
 - (IBAction)backButtonTapped
@@ -65,4 +106,52 @@
 	self.scannerLightImageView.image = (UIImage *)self.scannerLightImageArray[self.scannerLightImageIndex];
 }
 
+-(void)didReceiveMemoryWarning
+{
+	[super didReceiveMemoryWarning];
+}
+
+- (void) viewDidAppear: (BOOL) animated
+{
+	// run the reader when the view is visible
+	[self.zBarReaderView start];
+}
+
+- (void) viewWillDisappear: (BOOL) animated
+{
+	[self.zBarReaderView stop];
+}
+
+-(void)dealloc
+{
+	[self cleanup];
+}
+
+- (void) cleanup
+{
+	self.zBarReaderView.readerDelegate = nil;
+	self.zBarReaderView = nil;
+}
+
+- (void) willRotateToInterfaceOrientation: (UIInterfaceOrientation) orient
+																 duration: (NSTimeInterval) duration
+{
+	// compensate for view rotation so camera preview is not rotated
+	[self.zBarReaderView willRotateToInterfaceOrientation: orient
+																			duration: duration];
+}
+
+- (void) readerView: (ZBarReaderView*) view
+		 didReadSymbols: (ZBarSymbolSet*) syms
+					fromImage: (UIImage*) img
+{
+	// do something useful with results
+	for(ZBarSymbol *sym in syms) {
+		NSLog(@"%@", sym.data);
+		break;
+	}
+}
+
 @end
+
+ 
